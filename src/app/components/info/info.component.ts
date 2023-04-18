122f66acf3dc483ce7898f2b511f0352037cbd8e
TZajas <20278748@studentmail.ul.ie>
2023-04-18 18:57:19 +0100

    added projects

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -9,4 +9,6 @@ export class InfoComponent {
   skillsFirst = ['Angular', 'Java', 'PHP'];
   skillsSecond = ['MySQL', 'C#', 'ADO'];
 
+  about = "Hey there! My name is Tomasz Zajas and I am a 3rd year Computer Systems student at the University of Limerick. I love web technologies! Currently I am learning Django and React.";
+
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,11 +9,34 @@ import { DOCUMENT } from '@angular/common';
 export class ProjectsComponent {
     constructor(@Inject(DOCUMENT) private document: Document) { }
 
+    safe_campus = {
+      name : "Safe Campus",
+      desc : "Android application that allows user to alert previously added contacts with a pre-set message and current location with the click of a button. Made using Java and Android Studio",
+      vid : "https://www.youtube.com/embed/G5pLbGvhgb0"
+    }
+
+    wired_in = {
+      name : "WiredIn",
+      desc : "LinkedIn like application specially catered for software engineers. Made as part of a group project using PHP, MySQL, and Bootstrap.",
+      vid : "https://www.youtube.com/watch?v=LVlDSzbrH5M"
+    }
+
+    dogs_ceo = {
+      name : "Dogs CEO API",
+      desc : "Angular Project that demonstrates understanding APIs and the Angular Framework",
+      vid : "https://www.youtube.com/watch?v=LVlDSzbrH5M"
+    }
+
+
     wiredIn() {
-      this.document.location.href = 'https://stackoverflow.com';
+      this.document.location.href = 'https://github.com/Dawid33/CS4116'
     }
 
     safeCampus() {
-      this.document.location.href = 'https://stackoverflow.com';
+      this.document.location.href = 'https://github.com/TZajas/CS4084-App-Dev';
+    }
+
+    dogsCEO() {
+      this.document.location.href = 'https://github.com/TZajas/Dogs-Ceo-API-Project';
     }
 }
